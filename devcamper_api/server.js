e07d5d0cfb645aa195ef1569a27711a2a70d4b58
Arthur <arthurd800@wp.pl>
2020-06-07 14:02:59 +0200

    added controllers/bootcamp routes/bootcamps, connected with DB

--- devcamper_api/server.js
+++ devcamper_api/server.js
@@ -1,4 +1,29 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
+// .env variables
+dotenv.config({ path: './config/config.env' });
 
+//Connect to database
+connectDB();
+
+//Route files
+const bootcamps = require('./routes/bootcamps');
+
+
+// initialize app with express
+const app = express();
+
+// Dev loggin middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+//Mount Routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port:${PORT}...`));
 
