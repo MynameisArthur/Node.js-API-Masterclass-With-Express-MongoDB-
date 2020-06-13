1999cb549ee1f49a8a1319b0f790e77499bc66e0
Arthur <arthurd800@wp.pl>
2020-06-13 08:46:34 +0200

    added roles and authorization

--- devcamper_api/controllers/auth.js
+++ devcamper_api/controllers/auth.js
@@ -38,6 +38,9 @@ exports.login = asyncHandler(async (req, res, next) => {
     sendTokenResponse(user, 200, res);
 });
 
+
+
+
 // Get token  from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
     // Create token
@@ -50,5 +53,17 @@ const sendTokenResponse = (user, statusCode, res) => {
     if (process.env.NODE_ENV === 'production') {
         options.secure = true;
     }
-    res.status(statusCode).cookie('token', token, options).json({ success: true, token });
+    res.status(statusCode).cookie('token', token, options).json({ success: true, token: token });
 };
+
+// @desc Get current logged in user
+// @route POST /api/v1/auth/me
+// @access Private
+
+exports.getMe = asyncHandler(async (req, res, next) => {
+    const user = await User.findById(req.user.id);
+    res.status(200).json({
+        success: true,
+        data: user
+    });
+});
